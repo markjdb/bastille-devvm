51db0000f484a3eca7a36428041b132173aedbad
Mark Johnston <markjdb@gmail.com>
2021-06-12 10:57:20 -0400

    Initial revision.

---
+++ build.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+set -e
+
+usage()
+{
+    cat >&2 <<__EOF__
+usage: $(basename $0) [-a] [-c <KERNCONF>] [-m <module list>]
+
+Build a kernel and install it into the VM.
+
+  -a:           Clean build.  By default the kernel is built with -DKERNFAST.
+  -c:           Use the specified kernel config.  The default is GENERIC.
+  -m:           Specify a (possibly empty) list of modules to build.  By
+                default all modules are built.
+__EOF__
+    exit 1
+}
+
+KERNFAST=-DKERNFAST
+KERNCONF=GENERIC
+
+while getopts ac:j:m:t: o; do
+    case "$o" in
+    a)
+        KERNFAST=
+        ;;
+    c)
+        KERNCONF=$OPTARG
+        ;;
+    m)
+        MODULES="MODULES_OVERRIDE=${OPTARG}"
+        ;;
+    *)
+        usage
+        ;;
+    esac
+    shift $((OPTIND - 1))
+done
+
+cd ${_FREEBSD_SRC_PATH}
+make ${JFLAG} buildkernel -s $KERNFAST KERNCONF=$KERNCONF $MODULES
+make ${JFLAG} installkernel KERNCONF=$KERNCONF $MODULES DESTDIR=${_FREEBSD_KERN_BUILDROOT}
+
+makefs -B little -S 512 -Z -o label=kernel -o version=2 /root/vm_kern.part \
+    ${_FREEBSD_KERN_BUILDROOT}
+mkimg -s gpt -f raw -S 512 -b ${_FREEBSD_BUILDROOT}/boot/pmbr \
+    -p freebsd-ufs/kernel:=/root/vm_kern.part \
+    -o ${_FREEBSD_KERN_VM_IMG}
+
+# TODO: force bhyve to restart. Doesn't seem like there's a good way to do
+# this at present, sending SIGTERM triggers poweroff and bhyvectl --force-reset
+# is not graceful.
---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+status=0
+while [ $status -eq 0 ]; do
+    bhyve -c 2 -m 4G \
+        -s 0,hostbridge \
+        -s 1,lpc \
+        -s 2,virtio-blk,${_FREEBSD_VM_IMG} \
+        -s 3,virtio-blk,${_FREEBSD_KERN_VM_IMG} \
+        -l com1,stdio \
+        -l bootrom,/usr/local/share/uefi-firmware/BHYVE_UEFI.fd \
+        devvm
+    status=$?
+done
