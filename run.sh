8c2ae1202c7066f30d3fa73ce78c922b2b02749b
Mark Johnston <markjdb@gmail.com>
2022-01-15 19:13:54 -0500

    Rebuild the kernel after a reboot

--- run.sh
+++ run.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+usage()
+{
+    echo "usage: $(basename $0) [-c <kernconf>]" >&2
+    exit 1
+}
+
+KERNCONF=GENERIC
+while getopts c: o; do
+    case $o in
+    c)
+        KERNCONF=$OPTARG
+        ;;
+    *)
+        usage
+        ;;
+    esac
+done
+
 status=0
 while [ $status -eq 0 ]; do
     bhyve -c 2 -m 4G -H -A \
@@ -12,4 +30,10 @@ while [ $status -eq 0 ]; do
         -l bootrom,/usr/local/share/uefi-firmware/BHYVE_UEFI.fd \
         devvm
     status=$?
+    if [ $status -eq 0 ]; then
+        sh build.sh -c $KERNCONF
+        if [ $? != 0 ]; then
+            break
+        fi
+    fi
 done
